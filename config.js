ac39b5a166e45bf26d924977381be1eddf5695cf
minhquans <minhquanle11tp@gmail.com>
2022-11-11 23:46:03 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,22 @@
+const CONFIG = {
+    introTitle: 'hế lô QUỲNH HOA nhó',
+    introDesc: ``,
+    btnIntro: 'Tiếp Tục',
+    title: ' Cậu có muốn mặc hoodie của tớ vào mùa đông này khumm',
+    desc: 'chỉ có hoặc không thoai nhó ',
+    btnYes: 'Có',
+    btnNo: 'Không ',
+    question:'Mai tớ mang qua cho cậu nhó ',
+    btnReply: 'Tiếp Tục',
+    reply: 'oke Minh Quân ',
+    mess: 'oke thoai mai đi chơi ngta mang qua choa',
+    messDesc: 'ib ngay choa Minh Quân để được thơm má online nhó',
+    btnAccept: 'Tiếp Tục',
+    messLink: 'https://www.facebook.com/profile.php?id=100037435576667', //link fb
+    bgLink: 'https://bom.to/qjAJcv', //link ảnh nền
+    iputBgLink: 'https://bom.to/CepMcU', //link iputBgLink
+    lookMeLink: 'https://huypham9205.github.io/tang-crush/img/lookMe.jpg', //link ảnh lookMe
+    soundLink: 'https://huypham9205.github.io/tang-crush/sound/sound.mp3', //link nhac
+    duckLink: 'https://huypham9205.github.io/tang-crush/sound/duck.mp3', //link aam thanh 1
+    swishLink: 'https://huypham9205.github.io/tang-crush/sound/Swish1.mp3', //link swish
+}
