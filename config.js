f36ce363da1186b4a4caf9ad26e376b47c3d9769
minhquans <91418487+minhquans@users.noreply.github.com>
2022-11-12 00:26:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'hế lô QUỲNH HOA nhó',
+    introTitle: 'Chào chị iu <3 ',
     introDesc: ``,
     btnIntro: 'Tiếp Tục',
     title: ' Cậu có muốn mặc hoodie của tớ vào mùa đông này khumm',
@@ -8,14 +8,14 @@ const CONFIG = {
     btnNo: 'Không ',
     question:'Mai tớ mang qua cho cậu nhó ',
     btnReply: 'Tiếp Tục',
-    reply: 'oke Minh Quân ',
+    reply: 'oke Minh Quân          ',
     mess: 'oke thoai mai đi chơi ngta mang qua choa',
     messDesc: 'ib ngay choa Minh Quân để được thơm má online nhó',
     btnAccept: 'Tiếp Tục',
     messLink: 'https://www.facebook.com/profile.php?id=100037435576667', //link fb
     bgLink: 'https://bom.to/qjAJcv', //link ảnh nền
     iputBgLink: 'https://bom.to/CepMcU', //link iputBgLink
-    lookMeLink: 'https://huypham9205.github.io/tang-crush/img/lookMe.jpg', //link ảnh lookMe
+    lookMeLink: 'https://i.imgur.com/opMbMaf.jpg', //link ảnh lookMe
     soundLink: 'https://huypham9205.github.io/tang-crush/sound/sound.mp3', //link nhac
     duckLink: 'https://huypham9205.github.io/tang-crush/sound/duck.mp3', //link aam thanh 1
     swishLink: 'https://huypham9205.github.io/tang-crush/sound/Swish1.mp3', //link swish
